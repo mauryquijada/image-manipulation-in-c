0e3a477ac721f51f1d8d7b54d1445b79f2fc82dd
Maury Quijada <maury.quijada@gmail.com>
2014-01-13 15:39:56 -0500

    Edit Makefile to compile lodepng, add to process.c that allows it to read in PNG files.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 OBJECTS = array_utility.o bilinear_interpolation.o fourier_filter.o \
 	frequency_filter.o gradient_filter.o histogram_equalization.o \
 	histograms.o manipulate_hsi.o median_filter.o process.o resize.o \
-	rotate.o
+	rotate.o lodepng.o
 
 all:	process
 
@@ -20,11 +20,12 @@ fourier_filter.o: fourier_filter.c
 frequency_filter.o: frequency_filter.c
 gradient_filter.o: gradient_filter.c
 histogram_equalization.o: histogram_equalization.c
-histograms.o: histograms.c histograms.h
+histograms.o: histograms.c histograms.h array_utility.h
+lodepng.o: lodepng.c lodepng.h
 manipulate_hsi.o: manipulate_hsi.c
 median_filter.o: median_filter.c
 process.o: process.c array_utility.h rotate.h bilinear_interpolation.h \
-  resize.h histograms.h
-resize.o: resize.c resize.h bilinear_interpolation.h
-rotate.o: rotate.c rotate.h bilinear_interpolation.h
+  resize.h histograms.h lodepng.h
+resize.o: resize.c resize.h bilinear_interpolation.h array_utility.h
+rotate.o: rotate.c rotate.h bilinear_interpolation.h array_utility.h
 
--- process.c
+++ process.c
@@ -6,17 +6,70 @@
 #include "rotate.h"
 #include "resize.h"
 #include "histograms.h"
+#include "lodepng.h"
 
 int main ()
 {
-	char panda_name[50];
-	float** panda;
-	int panda_M, panda_N;
+	int action = 0;
 
-	printf("Hello world!");
+	char* filename;
+  	printf("Type in the name of the PNG file you would like to process.\n");
+  	scanf("%s", filename);
 
-	strcpy(panda_name, "panda.pgm");
+  	// Load the PNG using LodePNG library.
+  	// Ignoring Alpha values for now.
+  	printf("Processing the file...\n\n\n");
+  	unsigned error;
+ 	unsigned char* image;
+  	unsigned width, height;
 
-    // Read in the black and white images.
-    panda = read_bw_image(panda_name, &panda_M, &panda_N);
+  	error = lodepng_decode32_file(&image, &width, &height, filename);
+  	if (error) {
+  		printf("Error (%u) loading file: %s\n", error, lodepng_error_text(error));
+  	}
+
+	// Then use array to load into a three-dimensional array of R, G, and B values.
+  	float*** img = alloc3df(3, height, width);
+
+  	int i, j;
+  	int r, g, b, a;
+  	for (i = 0; i < height; i++) {
+  		for (j = 0; j < width; j++) {
+  			img[0][i][j] = image[4 * i * width + 4 * j + 0]; // Red.
+		    img[1][i][j] = image[4 * i * width + 4 * j + 1]; // Green.
+		    img[2][i][j] = image[4 * i * width + 4 * j + 2]; // Blue.
+		    // image[4 * i * width + 4 * j + 3]; is used to access alpha.
+  		}
+  	}
+
+  	while (action > -1) {
+  		printf("Select how you would like to process this image: \n");
+	  	printf("1) Generate a density histogram for the image.\n");
+	  	printf("2) Generate a cumulative histogram for the image.\n");
+	  	printf("3) Improve the image's contrast.\n");
+	  	printf("4) Resize an image's width and height by certain factors.\n");
+	  	printf("5) Rotate an image by some angle (in degrees).\n");
+	  	printf("6) Perform histogram equalization on the image.\n");
+	  	printf("7) Rotate the image's hue.\n");
+	  	printf("8) Increase the intensity of the image.\n");
+	  	printf("9) Increase the color saturation of the image.\n");
+	  	printf("10) Perform low-pass filtering on an image using a kernel.\n");
+	  	printf("11) Perform high-pass filtering on an image using a kernel.\n");
+	  	printf("12) Perform low-pass or high-pass filtering us a Fourier");
+	  	printf("transform.\n");
+	  	printf("13) Create a gradient image that accentuates changes in color");
+	  	printf("based on input.\n");
+	  	printf("14) Clean up an image using a median filter.\n");
+	  	printf("15) Detect edges in an image.\n");
+
+
+	  	printf("Type in the number of the action you're interested in (-1 to exit): ");
+	  	scanf("%d", &action);
+	  	printf("\n\n\n");
+  	}
+
+
+  	write_color_image("blah.ppm", img, height, width, 1);
+
+  	dealloc3df(img, 3, height, width);
 }
