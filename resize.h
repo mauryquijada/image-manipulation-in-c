914096f8ff622628a3dfa2bc8b6d010d1b84ca50
Maury Quijada <maury.quijada@gmail.com>
2014-01-12 23:15:42 -0500

    Add preliminary versions of resize, rotate, and interpolation.

---
+++ resize.h
@@ -0,0 +1,60 @@
+#ifndef RESIZE_H
+#define RESIZE_H
+
+#include "bilinear_interpolation.h"
+
+float** resize (float** input, int M_in, int N_in)
+{
+    // Prompt the user.
+    float vert_resize_factor, horiz_resize_factor;
+    printf("By what factor do you want to scale your image horizontally?\n");
+    scanf("%f", &horiz_resize_factor);
+    printf("By what factor do you want to scale your image vertically?\n");
+    scanf("%f", &verti_resize_factor);
+
+    printf("Resizing...\n");
+
+    // Figure out the dimensions of the new, resized image and define the
+    // new array.
+    int M_out_resized = M_in * verti_resize_factor;
+    int N_out_resized = N_in * horiz_resize_factor;
+    float** output = alloc2df(M_out_resized, N_out_resized);
+
+    // Loop through each pixel of the new image and interpolate the pixels
+    // based on what's in the input image.
+    int i, j;
+    for (i = 0; i < M_out_resized; ++i) {
+        for (j = 0; j < N_out_resized; ++j) {
+            // Figure out how far down or across (in percentage) we are with
+            // respect to the original image.
+            float vertical_position = i * ( (float) M_in / M_out_resized);
+            float horizontal_position = j * ( (float) N_in / N_out_resized);
+
+            // Figure out the four locations (and then, four pixels)
+            // that we must interpolate from the original image.
+            int top = floor(vertical_position);
+            int bottom = top + 1;
+            int left = floor(horizontal_position);
+            int right = left + 1;
+
+            // Check if any of the four locations are invalid. If they are,
+            // simply access the valid adjacent pixel.
+            if (bottom >= M_in) {
+                bottom = top;
+            }
+            if (right >= N_in) {
+                right = left;
+            }
+
+            // Interpolate the pixel according to the dimensions
+            // set above and set the resulting pixel.
+            float interpolated = bilinearly_interpolate(top, bottom, left,
+                right, horizontal_position, vertical_position, input);
+            output[i][j] = interpolated;
+        }
+    }
+
+    return output;
+}
+
+#endif
---
+++ rotate.h
@@ -0,0 +1,57 @@
+#ifndef ROTATE_H
+#define ROTATE_H
+
+#include "bilinear_interpolation.h"
+
+float** rotate (float** input, int M_in, int N_in)
+{
+    // Prompt the user.
+    printf("By how many degrees CCW do you want to rotate your image?\n");
+    scanf("%f", &rotation_factor);
+    printf("Rotating the input file...\n");
+
+    // Prepare the output files.
+    output = alloc2df(M_in, N_in);
+
+    // Define the center of the image.
+    int vertical_center = floor(M_out_rotated / 2);
+    int horizontal_center = floor(N_out_rotated / 2);
+
+    // Loop through each pixel of the new image, select the new vertical
+    // and horizontal positions, and interpolate the image to make the change.
+    int row, col;
+    for (row = 0; row < M_out_rotated; ++row) {
+        printf(" .");
+        for (col = 0; col < N_out_rotated; ++col) {
+            //    // Figure out how rotated we want the image.
+            double angle = rotation_factor * (double) Pi / 180;
+            float vertical_position = (float) cos(angle) *
+                (row - vertical_center) + sin(angle) * (col - horizontal_center)
+                + vertical_center;
+            float horizontal_position = (float) -sin(angle) *
+                (row - vertical_center) + cos(angle) * (col - horizontal_center)
+                + horizontal_center;
+
+            // Figure out the four locations (and then, four pixels)
+            // that we must interpolate from the original image.
+            int top = floor(vertical_position);
+            int bottom = top + 1;
+            int left = floor(horizontal_position);
+            int right = left + 1;
+
+            // Check if any of the four locations are invalid. If they are,
+            // skip interpolating this pixel. Otherwise, interpolate the
+            // pixel according to the dimensions set above and set the
+            // resulting pixel.
+            if (top >= 0 && bottom < M_in && left >= 0 && right < N_in ) {
+                float interpolated = interpolate(top, bottom, left, right,
+                    horizontal_position, vertical_position, greyscale_in);
+                output[row][col] = interpolated;
+            }
+        }
+    }
+
+    return output;
+}
+
+#endif
